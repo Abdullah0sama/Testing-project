fe68f104ef740b10b52a6787b3dfda21856abf9f
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2023-04-08 02:34:22 +0200

    adding the right way of extracting the jar file and all the remaining implementation of all functions

--- src/main/java/org/openjfx/Controller.java
+++ src/main/java/org/openjfx/Controller.java
@@ -12,6 +12,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class Controller implements Initializable {
 
@@ -96,28 +97,66 @@ public class Controller implements Initializable {
         // other n lines (after the first one) for all the students in this course
         ArrayList<String> linesInArrList = new ArrayList<>(Arrays.asList(xml.split("[\n|\r]+")));
 
-        parseCourseLine("");
-        parseStudentLines("");
+        ArrayList<SubjectGrades> subjects = parseFile(xml);
 
         // used to collect the output string to the gui
-        StringBuilder sb = new StringBuilder();
-        for (String element: linesInArrList){
-            sb.append(element).append("\n\n");
-        }
-
+//        StringBuilder sb = new StringBuilder();
+//        for (String element: linesInArrList){
+//            sb.append(element).append("\n\n");
+//        }
         // show your method result in xmlOut
-        xmlOut = sb.toString();
+        xmlOut = subjectsOutput(subjects);
         resultTA.setText(xmlOut);
     }
 
-    String parseCourseLine(String firstLine) {
 
-        return " ";
+    String subjectsOutput(ArrayList<SubjectGrades> subjectGrades) {
+        ArrayList<String> outputString = new ArrayList<>();
+        outputString.add("Subject Name: " + subjectGrades.get(0).getSubjectName());
+        outputString.add("Max Mark: " + 100);
+
+        for(int i = 0; i < subjectGrades.size(); ++i) {
+            SubjectGrades subject = subjectGrades.get(i);
+            String currentSubject = subject.getStudentName() + " " + subject.getStudentID() + " " + subject.gpaCalculator() + " " + subject.gradeCalculator();
+            outputString.add(currentSubject);
+        }
+
+        return String.join("\n", outputString);
     }
+    ArrayList<SubjectGrades> parseFile(String fileContent) {
+        ArrayList<String> linesInArrList = new ArrayList<>(Arrays.asList(xml.split("[\n|\r]+")));
+        String[] subjectInfo = linesInArrList.get(0).split(",");
+        String subjectName = subjectInfo[0];
+        String subjectCode = subjectInfo[1];
+        String subjectMaxMark = subjectInfo[2];
 
-    String parseStudentLines(String consecutive) {
+        ArrayList<SubjectGrades> subjectsGrades = new ArrayList<>();
 
-        return " ";
+        for (int i = 1; i < linesInArrList.size(); ++i) {
+            subjectsGrades.add(parseStudent(subjectName, subjectCode, linesInArrList.get(i)));
+        }
+
+        return subjectsGrades;
+    }
+    SubjectGrades parseStudent(String subjectName, String subjectCode, String studentTxt) {
+
+        SubjectGrades student = new SubjectGrades();
+
+        student.setSubjectName(subjectName);
+        student.setSubjectCode(subjectCode);
+        ArrayList<String> studentData = new ArrayList<>(Arrays.asList(studentTxt.split(",")));
+
+        if(studentData.size() != 6) {
+            throw new IllegalArgumentException("Some student arguments are missing!");
+        }
+
+        student.setStudentName(studentData.get(0));
+        student.setStudentID(studentData.get(1));
+        student.setActivities(Integer.parseInt(studentData.get(2)));
+        student.setOralMark(Integer.parseInt(studentData.get(3)));
+        student.setMidterm(Integer.parseInt(studentData.get(4)));
+        student.setFinal(Integer.parseInt(studentData.get(5)));
+        return student;
     }
 
 
---
+++ src/main/java/org/openjfx/SubjectGrades.java
@@ -0,0 +1,189 @@
+package org.openjfx;
+
+public class SubjectGrades {
+    String SubjectName;
+    String SubjectCode;
+    String StudentName;
+    String StudentID;
+    int OralMark,Midterm,Final,Activities;
+
+    public String getSubjectName() {
+        return SubjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        if (!(subjectName.matches("^[a-zA-Z]+( [a-zA-Z]+)*$"))) {
+            throw new IllegalArgumentException("Name must contain only alphabetic characters and spaces, and should not start with a space.");
+        }
+
+        SubjectName = subjectName;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        if(subjectCode.length() != 7 && subjectCode.length() != 6){
+            System.out.println("Subject code should be 6 or 7 letters");
+        }
+        String alpaCode = subjectCode.substring(0,2);
+        String numCode = subjectCode.substring(3,5);
+        if(!alpaCode.matches("[a-zA-Z]+")){
+            System.out.println("The first 3 letters should be alphabet");
+        } else if (!numCode.matches(("[0-9]+"))) {
+            System.out.println("From fourth to Sixth letters should be numbers");
+        }else if ((subjectCode.length() == 7) && (subjectCode.indexOf("s") != 7 )){
+            System.out.println("The seventh letter should be (S)");
+        }
+        SubjectCode = subjectCode;
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(String studentName) {
+        if (!(studentName.matches("^[a-zA-Z]+( [a-zA-Z]+)*$"))) {
+            throw new IllegalArgumentException("Name must contain only alphabetic characters and spaces, and should not start with a space.");
+        }
+        StudentName = studentName;
+    }
+
+    public String getStudentID() {
+        return StudentID;
+    }
+
+    public void setStudentID(String studentID) {
+       if (!(studentID.length() == 8 && studentID.substring(0, 7).matches("\\d+"))) {
+            System.out.println("The first seven characters are not all numbers.");
+        }
+        StudentID = studentID;
+    }
+
+    public int getOralMark() {
+        return OralMark;
+    }
+
+    public void setOralMark(int oralMark) {
+        if (!(oralMark >= 0 && oralMark <= 10)) {
+            System.out.println("Mark must be between 0 and 10.");
+        }
+        OralMark = oralMark;
+
+    }
+
+    public int getMidterm() {
+        return Midterm;
+    }
+
+    public void setMidterm(int midterm) {
+        if (!(midterm >= 0 && midterm <= 20)) {
+            System.out.println("Mark must be between 0 and 20.");
+        }
+        Midterm = midterm;
+    }
+
+    public int getFinal() {
+        return Final;
+    }
+
+    public void setFinal(int aFinal) {
+        if (!(aFinal >= 0 && aFinal <= 60)) {
+            System.out.println("Mark must be between 0 and 60.");
+        }
+        Final = aFinal;
+    }
+
+    public int getActivities() {
+        return Activities;
+    }
+
+    public void setActivities(int activities) {
+        if (!(activities >= 0 && activities <= 10)) {
+            System.out.println("Mark must be between 0 and 10.");
+        }
+        Activities = activities;
+    }
+
+
+    public static String calculateGrade(int mark) {
+        if (mark >= 97 && mark <= 100) {
+            return "A+";
+        } else if (mark >= 93 && mark < 97) {
+            return "A";
+        } else if (mark >= 89 && mark < 93) {
+            return "A-";
+        } else if (mark >= 84 && mark < 89) {
+            return "B+";
+        } else if (mark >= 80 && mark < 84) {
+            return "B";
+        } else if (mark >= 76 && mark < 80) {
+            return "B-";
+        } else if (mark >= 73 && mark < 76) {
+            return "C+";
+        } else if (mark >= 70 && mark < 73) {
+            return "C";
+        } else if (mark >= 67 && mark < 70) {
+            return "C-";
+        } else if (mark >= 64 && mark < 67) {
+            return "D+";
+        } else if (mark >= 60 && mark < 64) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+    public static double calculateGPA(int mark) {
+        if (mark >= 97 && mark <= 100) {
+            return 4.0;
+        } else if (mark >= 93 && mark < 97) {
+            return 4.0;
+        } else if (mark >= 89 && mark < 93) {
+            return 3.7;
+        } else if (mark >= 84 && mark < 89) {
+            return 3.3;
+        } else if (mark >= 80 && mark < 84) {
+            return 3.0;
+        } else if (mark >= 76 && mark < 80) {
+            return 2.7;
+        } else if (mark >= 73 && mark < 76) {
+            return 2.3;
+        } else if (mark >= 70 && mark < 73) {
+            return 2.0;
+        } else if (mark >= 67 && mark < 70) {
+            return 1.7;
+        } else if (mark >= 64 && mark < 67) {
+            return 1.3;
+        } else if (mark >= 60 && mark < 64) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
+    }
+
+    public String gradeCalculator (){
+//        this.OralMark = OralMark;
+//        this.Midterm = Midterm;
+//        this.Final = Final;
+//        this.Activities = Activities;
+        int Full = OralMark + Midterm + Final + Activities;
+        if(Full > 100){
+            throw new IllegalArgumentException("Input value cannot be greater than 100");
+        }
+        String result = calculateGrade(Full);
+        return result;
+    }
+
+
+    public double gpaCalculator (){
+//        this.OralMark = OralMark;
+//        this.Midterm = Midterm;
+//        this.Final = Final;
+//        this.Activities = Activities;
+        int Full = OralMark + Midterm + Final + Activities;
+        if(Full > 100){
+            throw new IllegalArgumentException("Input value cannot be greater than 100");
+        }
+        double result = calculateGPA(Full);
+        return result;
+    }
+}
+
+
